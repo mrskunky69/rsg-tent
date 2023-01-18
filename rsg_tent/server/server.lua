e80b51ca13a65a3cd07721638b33ed685520a7f2
mrskunky69 <40913528+mrskunky69@users.noreply.github.com>
2023-01-18 19:19:31 +0000

    Add files via upload

--- rsg_tent/server/server.lua
+++ rsg_tent/server/server.lua
@@ -3,30 +3,29 @@ local xSound = exports.xsound
 local isPlaying = false
 
 RSGCore.Functions.CreateUseableItem("tent", function(source, item)
-	local src = source
-	local Player = RSGCore.Functions.GetPlayer(src)
-	TriggerClientEvent('rsg_tent:client:placeDJEquipment', src)
-	Player.Functions.RemoveItem('tent', 1)
-	TriggerClientEvent('inventory:client:ItemBox', src, RSGCore.Shared.Items['tent'], "remove")
+    local Player = RSGCore.Functions.GetPlayer(source)
+	if Player.Functions.RemoveItem(item.name, 1, item.slot) then
+        TriggerClientEvent("rsg_tent:client:placeDJEquipment", source, item.name)
+    end
 end)
 
 
-RegisterNetEvent('rsg_mobiledj:server:pickedup', function(entity)
+RegisterNetEvent('rsg_tent:server:pickedup', function(entity)
     local src = source
     xSound:Destroy(-1, tostring(entity))
 end)
 
 
-RegisterNetEvent('rsg_mobiledj:Server:RemoveItem', function(item, amount)
+RegisterNetEvent('rsg_tent:Server:RemoveItem', function(item, amount)
     local src = source
     local Player = RSGCore.Functions.GetPlayer(src)
     Player.Functions.RemoveItem(item, amount)
 end)
 
 RegisterServerEvent('rsg_tent:server:pickeupdecks')
-AddEventHandler('rsg_tent:server:pickeupdecks', function()
+AddEventHandler('rsg_cookit:server:pickeupdecks', function()
 	local src = source
     local Player = RSGCore.Functions.GetPlayer(src)
-	Player.Functions.AddItem('tent', 1)
-	TriggerClientEvent('inventory:client:ItemBox', src, RSGCore.Shared.Items['tent'], "add")
+	Player.Functions.AddItem('tent', 0)
+	TriggerClientEvent('inventory:client:ItemBox', src, RSGCore.Shared.Items['tent'], "remove")
 end)
