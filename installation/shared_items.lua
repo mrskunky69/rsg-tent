42975d0a178231a3dedf956e8edda0f10b5093c8
mrskunky69 <40913528+mrskunky69@users.noreply.github.com>
2023-09-30 14:57:59 +0100

    Update shared_items.lua

--- installation/shared_items.lua
+++ installation/shared_items.lua
@@ -1,2 +1,2 @@
-	-- mobile dj
+	
 	['tent']					= {['name'] = 'tent', 			  	  		['label'] = 'tent', 				['weight'] = 500, 		['type'] = 'item', 		['image'] = 'tent.png', 				['unique'] = false, 		['useable'] = true, 	['shouldClose'] = true,		['combinable'] = nil,   ['description'] = 'temp camp'},
